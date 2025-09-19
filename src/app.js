f3d6c4a52071e0e3cc524c8792d8c854785f16dc
vinay1945 <vinaykumarporandla19@gmail.com>
2025-09-19 23:53:30 +0530

    feat: add utils file and healthcheck to initial API setup

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// cors configurations
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+//  import the routes
+import healthCheckRouter from "./routes/healthcheck.route.js"
+
+
+// use routes
+app.use('/api/v1/healthcheck', healthCheckRouter)
+
+
+export default app;
